c8a25662457ae57e0fecead82488d3f6738b35b9
Yauheni Chupilin <e.chupilin@gmail.com>
2017-04-18 21:24:01 +0300

    add

---
+++ src/main/java/by/training/wolterskluwer/App.java
@@ -0,0 +1,13 @@
+package by.training.wolterskluwer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Test file copy plugin." );
+    }
+}
